3ed768ddee27d3eb3dfe620ccf71d242932f76c3
Danny Huang <huangdong@gmail.com>
2021-08-11 22:42:16 -0400

    fix bug

--- WangWangBot/filters/admin.py
+++ WangWangBot/filters/admin.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher.filters import BoundFilter
 from typing import Union
+from WangWangBot.utils.logger import log
 
 from ..config import Config
 
@@ -13,11 +14,12 @@ class AdminFilter(BoundFilter):
         self.in_admins = in_admins
 
     async def check(self, message: Union[types.Message,types.CallbackQuery]) -> bool:
-        if message.from_user.id in ADMINS:
+        if str(message.from_user.id) in ADMINS:
             return True
         else:
             if isinstance(message, types.Message):
                 await message.reply('你不是管理员，小心他发现你在搞小动作')
             elif isinstance(message, types.CallbackQuery):
                 await message.answer('你不是管理员，小心他发现你在搞小动作',show_alert=True)
+            log.warning(f'{message.from_user} tried to use admin filter')
             return False

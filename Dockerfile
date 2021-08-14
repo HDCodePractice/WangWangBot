082921f9d4222e2f9e3dbee01a81babc35d1672b
Danny Huang <huangdong@gmail.com>
2021-08-14 18:04:34 -0400

    让项目不是必须要使用doppler

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,7 @@ RUN apt-get update && \
     apt upgrade -y && \
     apt-get install -qy curl && \
     apt-get install -qy libsodium-dev && \
-    curl -fsSL https://get.docker.com | sh && \
-    (curl -Ls https://cli.doppler.com/install.sh || wget -qO- https://cli.doppler.com/install.sh) | sh
+    curl -fsSL https://get.docker.com | sh 
 RUN cd /
 COPY . /WangWangBot/
 RUN cd WangWangBot
@@ -13,5 +12,4 @@ WORKDIR /WangWangBot
 RUN SODIUM_INSTALL=system pip install pynacl
 RUN pip3 install -U -r requirements.txt
 WORKDIR /data
-ENTRYPOINT [ "doppler", "run", "--" ]
 CMD ["python3", "/WangWangBot/main.py"]
--- WangWangBot/config.py
+++ WangWangBot/config.py
@@ -1,8 +1,45 @@
+from email.policy import default
 import os
 from environs import Env
+from io import StringIO
+from dotenv import load_dotenv
+
+import requests
+from base64 import b64encode
+
+
+def get_doppler_env(token):
+    token_b64 = b64encode(f"{token}:".encode()).decode()
+
+    url = "https://api.doppler.com/v3/configs/config/secrets/download"
+
+    querystring = {"format":"env"}
+
+    headers = {
+        "Accept": "application/json",
+        "Authorization": f"Basic {token_b64}"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    if response.status_code == 200:
+        return response.text
+    return ""
 
 env = Env()
 env.read_env(f"{os.getcwd()}/local.env")
+
+e = os.environ
+
+def set_env_by_doppler():
+    pass
+
+doppler_token = env.str('DOPPLER_TOKEN', default='')
+
+if len(doppler_token) > 0 :
+    response = get_doppler_env(doppler_token)
+    if len(response) > 0:
+        config = StringIO(response)
+        load_dotenv(stream=config)
 
 class Config:
     WORKDIR=os.getcwd()
